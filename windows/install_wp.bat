b18f9812161885a33b8f8ffaab0ddae4f3f92872
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-11-09 13:48:33 -0500

    Update install_wp.bat

--- windows/install_wp.bat
+++ windows/install_wp.bat
@@ -2,3 +2,8 @@
 echo.
 echo Setting up Windows IIS web server
 dism /norestart /online /enable-feature /featurename:IIS-WebServer
+timeout /t 3 /nobreak
+echo.
+echo Downloading and installing WordPress
+curl https://wordpress.org/latest.zip
+tar -xf latest.zip
